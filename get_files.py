95310f66e4f2f2ee16e919e25480593e416272fe
Jerzy de Mezer <jerzy.demezer@laboratorium.ee>
2015-07-11 15:50:02 +0200

    refactor getter

--- get_files.py
+++ get_files.py
@@ -1,19 +1,16 @@
 __author__ = 'jerzydem'
 import sys
 
-def get_page(name, index):
+def prepare_page_url(name):
+    import urllib
+    refine_name = name.replace('?', ' ').replace('-', ' ').replace('(', ' ').replace(')', ' ');
+    # add name values to table
+    names_table = refine_name.split()
 
-    import urllib.request
-    import urllib.parse
-    import sys
-    import time
-    from slugify import slugify
-
-
-    names_table = name.replace('?', ' ').replace('-', ' ').replace('(', ' ').replace(')', ' ').split()
-
+    # prepare url query
     name_query = ''
     for token in names_table:
+        # ignore token "ksiądz"
         if token != "ksiądz":
             name_query += urllib.parse.quote(token)
             name_query += "+"
@@ -21,25 +18,45 @@ def get_page(name, index):
 
     encoded_url = "https://bs.sejm.gov.pl/F/?func=find-b&request=" + name_query + \
                   "&find_code=WPN&adjacent=N&x=27&y=6&local_base=ars10"
+    return encoded_url
 
-    response = urllib.request.urlopen(encoded_url)
+def get_page(name, index):
+    # init page url preparation and save html in destination folder
+
+    import urllib.request
+    import urllib.parse
+    import sys
+    import time
+    from slugify import slugify
+
+    DESTINATION_FOLDER = 'results3/'
+
+    url = prepare_page_url(name)
+
+    response = urllib.request.urlopen(url)
     web_content = response.read()
     size = sys.getsizeof(web_content)
+
+    # 5 seconds timeout to limit server pauses
     time.sleep(5)
 
+    # detect if server is down. When server is down, result pages are very small
     if size < 1000:
+        # BS Server down
         print('SERVER OFF {:>4} {} SIZE: {}'.format(index, encoded_url, size))
         time.sleep(5)
         get_page(name, index)
     else:
-        print(index, " Name: ", name, " ", encoded_url)
-        f = open('results3/' + str(index) + "_" + slugify(name) + ".html", 'wb')
+        # Page size is enough. Save page
+        print(index, " Name: ", name, " ", url)
+        f = open(DESTINATION_FOLDER + str(index) + "_" + slugify(name) + ".html", 'wb')
         f.write(web_content)
         f.close
 
 
 
 def csv_get_all_pages(csv_file, start_line):
+    # get names of members of Sejm from csv file and init single page getter
     import csv
 
     input_file = open(csv_file)
@@ -48,7 +65,7 @@ def csv_get_all_pages(csv_file, start_line):
 
     rows_num = 0
     for row in reader:
-        # ignore row if its lower then start_line
+        # ignore row if its number is lower then start_line (to start from specific number)
         if rows_num > start_line-1:
             name = row[1]
             get_page(name, rows_num)
@@ -57,25 +74,18 @@ def csv_get_all_pages(csv_file, start_line):
     input_file.close()
 
 
-# INITIALIZATION
+def init_getter():
+    # set init values and start getter
 
-# CONSTANT VALUES
-DEPUTIES_CSV = "files/sejm_ustawodawczy.csv"
-START_LINE = 209
+    # CONSTANT VALUES
+    DEPUTIES_CSV = "files/sejm_ustawodawczy.csv"
+    START_LINE = 0
 
+    # INLINE ARGUMENTS
+    # use inline arguments if exist
+    start_row = int(sys.argv[1]) if len(sys.argv) > 1 else START_LINE
+    input_csv = sys.argv[2] if len(sys.argv) > 2 else DEPUTIES_CSV
 
-# USE INIT INLINE ARGUMENTS
-# TODO add short form of "if"
-# TODO - test it
-if len(sys.argv) > 1:
-    start_row = sys.argv[1]
-    if len(sys.argv) > 2:
-        input_csv = sys.argv[2]
-    else:
-        input_csv = DEPUTIES_CSV
-else:
-    start_row = START_LINE
+    csv_get_all_pages(input_csv, start_row)
 
-
-
-csv_get_all_pages(input_csv, start_row)
+init_getter()
